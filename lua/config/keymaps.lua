c410d4f9587976b8dbe1d1b5bc44687911ad8e1e
isnotvinicius <vinicius.oliveira3301@gmail.com>
2023-12-21 15:54:28 -0300

    keymaps refactor | colorscheme changed

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,6 +5,9 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
 
@@ -20,6 +23,3 @@ end)
 vim.keymap.set("n", "<C-s>", function()
   ui.nav_file(4)
 end)
-
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
