b62b1c9978a7188033789ed52963ad37c1149584
isnotvinicius <vinicius.oliveira3301@gmail.com>
2023-12-22 14:26:20 -0300

    changed colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,33 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = { style = "night", transparent = true },
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+    config = function()
+      return {
+        require("rose-pine").setup({
+          variant = "auto",
+          dark_variant = "main",
+
+          disable_background = true,
+          disable_nc_background = true,
+          disable_float_background = true,
+          disable_italics = true,
+
+          groups = {
+            -- background = "pine", --"#133340", --"#255159",
+            -- panel = "surface",   -- "#133340",
+            border = "pine", --"pine",
+          },
+        }),
+      }
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
   },
 }
