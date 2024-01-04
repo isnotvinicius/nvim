f936437e091f43a9be2fe72ba8d45d906e1208e5
isnotvinicius <vinicius.oliveira3301@gmail.com>
2024-01-04 14:56:20 -0300

    theme changes (again...)

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,26 +1,7 @@
 return {
   {
     "folke/tokyonight.nvim",
-
-    opts = {
-      -- transparent = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
-    },
-  },
-
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "tokyonight",
-    },
-  },
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      background_colour = "#000000",
-    },
+    lazy = true,
+    opts = { style = "night" },
   },
 }
