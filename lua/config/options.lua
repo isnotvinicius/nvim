dc12fec76c66dadfd4fca3d772ec3d51120b3bef
isnotvinicius <vinicius.oliveira3301@gmail.com>
2025-01-13 15:18:58 -0300

    defined tabspace as 2

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -19,7 +19,7 @@ vim.opt.relativenumber = false
 
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
