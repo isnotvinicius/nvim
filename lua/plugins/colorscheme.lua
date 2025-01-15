5168dd86988717d604ac669cc0de6257030a6246
isnotvinicius <vinicius.oliveira3301@gmail.com>
2025-01-15 18:50:31 -0300

    changed colorscheme to rose-pine

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function()
+      require("rose-pine").setup({
+        variant = "moon",
+        dark_variant = "moon",
+        styles = {
+          bold = true,
+          italic = false,
+          transparency = false,
+        },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
