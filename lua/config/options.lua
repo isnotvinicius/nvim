a4131e63221d4f0c424b1a595fb8acf7782f3935
Vinícius Oliveira <44581255+isnotvinicius@users.noreply.github.com>
2025-05-06 13:27:43 -0300

    Update options.lua

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,12 +1,10 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options before
---
+-- Add any additional options here
 
-local opt = vim.opt
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
--- Tab count
-opt.tabstop = 2
-
--- Shift size
-opt.shiftwidth = 2
+vim.opt.smartindent = true
