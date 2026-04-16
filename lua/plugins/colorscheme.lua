732a85682ce2248f5c25fd1682c2449ce74ed160
isnotvinicius <vinicius.oliveira3301@gmail.com>
2026-04-16 17:02:47 -0300

    fix: updated plugins to newer versions

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,72 +1,69 @@
 function ColorMyPencils(color)
-  color = color or "catppuccin-mocha"
-  vim.cmd.colorscheme(color)
+    color = color or "night-owl" -- defina UM padrão
 
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    vim.cmd.colorscheme(color)
+
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
 
-  {
-    "erikbackman/brightburn.vim",
-  },
+    { "erikbackman/brightburn.vim" },
 
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    opts = {},
-    config = function()
-      ColorMyPencils()
-    end,
-  },
-  {
-    "folke/tokyonight.nvim",
-    config = function()
-      require("tokyonight").setup({
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-        transparent = true, -- Enable this to disable setting the background color
-        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = false },
-          keywords = { italic = false },
-          -- Background styles. Can be "dark", "transparent" or "normal"
-          sidebars = "dark", -- style for sidebars, see below
-          floats = "dark", -- style for floating windows
+    {
+        "oxfist/night-owl.nvim",
+        name = "night-owl",
+        config = function()
+            require("night-owl").setup({
+                italics = false
+            })
+            ColorMyPencils("night-owl")
+        end,
+    },
+
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000, -- garante que carrega primeiro
+        config = function()
+            require("tokyonight").setup({
+                style = "storm",
+                transparent = true,
+                terminal_colors = true,
+                styles = {
+                    comments = { italic = false },
+                    keywords = { italic = false },
+                    sidebars = "dark",
+                    floats = "dark",
+                },
+            })
+        end,
+    },
+
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require("rose-pine").setup({
+                disable_background = true,
+                styles = {
+                    italic = false,
+                },
+            })
+        end,
+    },
+
+    {
+        "craftzdog/solarized-osaka.nvim",
+        name = "solarized-osaka",
+        opts = {
+            transparent = true,
         },
-      })
-    end,
-  },
+    },
 
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        disable_background = true,
-        styles = {
-          italic = false,
-        },
-      })
-
-      ColorMyPencils()
-    end,
-  },
-
-  {
-    "craftzdog/solarized-osaka.nvim",
-    name = "solarized-osaka",
-    lazy = true,
-    priority = 1000,
-    opts = function()
-      return {
-        transparent = true,
-      }
-    end,
-  },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+    },
 }
